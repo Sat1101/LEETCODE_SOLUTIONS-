feaf9385e7d141ed980f25ac87f8c0ab81a9b402
Satyam Agrawal <satyamagrawal1101@gmail.com>
2026-09-17 22:39:05 +0530

    Time: 0 ms (100%), Space: 48.3 MB (69.33%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int n=arr.length;
+        int i=0,j=n-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]>target)j=mid-1;
+            else if(arr[mid]<target)i=mid+1;
+            else  return mid;
+        }
+        return -1;
+    }
+}
